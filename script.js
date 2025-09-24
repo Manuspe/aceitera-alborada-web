1d9cd8f30571da0297175e26f3fdc1d93c48dc7e
Emanuel <manusperanza9@gmail.com>
2025-09-24 16:01:35 -0300

    Update script.js with smooth scroll functionality

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 // Smooth Scroll
+// Evita error con href="#"
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
+        const target = this.getAttribute('href');
+        if (target.length > 1) { // Solo si hay algo después del "#"
+            e.preventDefault();
+            const el = document.querySelector(target);
+            if (el) {
+                el.scrollIntoView({ behavior: 'smooth' });
+            }
+        }
     });
 });
